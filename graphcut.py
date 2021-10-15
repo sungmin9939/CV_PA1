608021ca3e7f1aa60c77f2066c84c732805cb580
sungmin <sungmin9939@naver.com>
2021-10-15 17:44:11 +0900

    graphcut

--- graphcut.py
+++ graphcut.py
@@ -1,15 +1,41 @@
 import imcut.pycut
 import numpy as np
+import cv2
+import sys
+from PIL import Image
+img = cv2.imread('./datasets/Emily-In-Paris-gray.png',cv2.IMREAD_GRAYSCALE)
+scribble = cv2.imread('./datasets/Emily-In-Paris-scribbles.png')
+scribble = scribble[:,:,2]
+gt = cv2.imread('./datasets/Emily-In-Paris-gt.png')
+gt = gt[:,:,2]
 
-im = np.random.random([5, 5, 1])
-im[:3, :3] += 1.
 
-seeds = np.zeros([5, 5, 1], dtype=np.uint8)
-seeds[:3,0] = 1  # foreground
-seeds[:3,4] = 2  # background
 
-gc = imcut.pycut.ImageGraphCut(im)
-gc.set_seeds(seeds)
+img = np.expand_dims(img, axis=-1)
+scribble = np.expand_dims(scribble, axis=-1)
+gc = imcut.pycut.ImageGraphCut(img)
+gc.set_seeds(scribble)
 gc.run()
+result = gc.segmentation.squeeze()
 
-print(gc.segmentation.squeeze())
+result_img = np.zeros(result.shape)
+inter = 0
+union = 0
+print(result_img.shape)
+for i in range(result.shape[0]):
+    for j in range(result.shape[1]):
+        if result[i][j] == gt[i][j] == 0:
+            continue
+        elif result[i][j] == gt[i][j] == 1:
+            union += 1
+            inter += 1
+            result_img[i][j] = 255
+        else:
+            union += 1
+            result_img[i][j] = 255 if result[i][j] else 0
+
+print("graph cut IoU: {}".format(inter/union))
+result_img = result_img.astype(np.int16)
+result_img = Image.fromarray(result_img)
+result_img.save('./results/result_graphcut_normal.png','PNG')
+result_img.show()
